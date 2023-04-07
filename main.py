efd01c84c74e07906dd60e0c1f1ba317e99d89a7
Rodolfo Helfenstein <44948279+RodolfoHelf@users.noreply.github.com>
2023-04-07 15:02:29 -0300

    API Handler

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from flask import Flask, jsonify
-import os
+import pickle
+import pandas as pd
+from flask             import Flask, request, Response
+from rossmann import Rossmann
 
-app = Flask(__name__)
+# loading model
+model = pickle.load(open("../parameter/xgb_tuned.pkl","rb"))
 
+# initialize API
+app = Flask( __name__ )
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route( '/rossmann/predict', methods=['POST'] )
+def rossmann_predict():
+    test_json = request.get_json()
+   
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+            
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+            
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+        
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+        
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+        
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+        
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        
+        return df_response
+        
+        
+    else:
+        return Reponse( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run( '0.0.0.0' )
