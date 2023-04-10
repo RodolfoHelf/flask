05761f23cada9e6c72b10ee91d69e4eafe76ae6f
Rodolfo Helfenstein <44948279+RodolfoHelf@users.noreply.github.com>
2023-04-10 08:04:05 -0300

    update file path

--- rossmann.py
+++ rossmann.py
@@ -8,11 +8,11 @@ import datetime
 class Rossmann( object ):
     def __init__( self ):
         self.home_path='/Estudos/Comunidade_DS/ds_em_produção'
-        self.competition_distance_scaler   = pickle.load( open( '../parameter/competition_distance_scaler.pkl', 'rb') )
-        self.competition_time_month_scaler = pickle.load( open( '../parameter/competition_time_month_scaler.pkl', 'rb') )
-        self.promo_time_week_scaler        = pickle.load( open( '../parameter/promo_time_week_scaler.pkl', 'rb') )
-        self.year_scaler                   = pickle.load( open( '../parameter/year_scaler.pkl', 'rb') )
-        self.store_type_scaler             = pickle.load( open( '../parameter/store_type_scaler.pkl', 'rb') )
+        self.competition_distance_scaler   = pickle.load( open( './parameter/competition_distance_scaler.pkl', 'rb') )
+        self.competition_time_month_scaler = pickle.load( open( './parameter/competition_time_month_scaler.pkl', 'rb') )
+        self.promo_time_week_scaler        = pickle.load( open( './parameter/promo_time_week_scaler.pkl', 'rb') )
+        self.year_scaler                   = pickle.load( open( './parameter/year_scaler.pkl', 'rb') )
+        self.store_type_scaler             = pickle.load( open( './parameter/store_type_scaler.pkl', 'rb') )
         
         
         
